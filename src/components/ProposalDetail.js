3892dc202c3da8e506f07c0f455186b6be6676bc
Camille Lagrama <camille.lagrama@starshotsoftware.com>
2019-01-25 22:39:21 +0800

    Fixed proposal details responsiveness

--- src/components/ProposalDetail.js
+++ src/components/ProposalDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Divider, Grid, Icon, Segment, Button, Progress, Image} from "semantic-ui-react";
+import { Divider, Grid, Icon, Segment, Button, Progress, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import hood from 'assets/hood.png';
 
@@ -37,14 +37,18 @@ const elders = [
     "shares": 33
   },
   {
-    "name": "Yan Agis",
-    "shares": 21
-  }
+    "name": "Todd Luch",
+    "shares": 33
+  },
+  {
+    "name": "Todd Luch",
+    "shares": 33
+  },
 ];
 
 const ProgressBar = ({ yes, no }) => (
   <>
-    <div style={{"position": "relative"}}>
+    <div style={{ "position": "relative" }}>
       <Progress percent={yes + no} color="red" size="big" style={{
         "position": "absolute",
         "top": "0",
@@ -64,25 +68,27 @@ const ProgressBar = ({ yes, no }) => (
 );
 
 const MemberAvatar = ({ name, shares }) => (
-  <Grid.Column textAlign="center">
+  <Grid.Column mobile={4} tablet={3} computer={3} textAlign="center" className="member_avatar" >
     <Link to={`/members/${name}`} className="uncolored">
       <Image src={hood} centered />
       <p className="name">{name}</p>
-      <p className="subtext">{shares} shares</p>
+      {/* <p className="subtext">{shares} shares</p> */}
     </Link>
   </Grid.Column>
 );
 
 export default (props) => (
   <div id="proposal_detail">
-    <div className="title">ETH Proposal</div>
-    <Grid columns={16}>
-      <Grid.Row>
-        <Divider vertical />
-
-        <Grid.Column width={6}>
-          <Segment className="transparent left box">
-            <div className="subtext">
+    <Grid centered columns={12}>
+      <Segment className="transparent box">
+        <Grid centered columns={10}  >
+          <Grid.Column mobile={16} tablet={16} computer={12}  >
+            <span className="title">ETH Proposal</span>
+          </Grid.Column>
+        </Grid>
+        <Grid centered columns={10}  >
+          <Grid.Column mobile={16} tablet={16} computer={4}  >
+            <div className="subtext description">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit,
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiut aliquip ex ea commodo consequat.
@@ -96,21 +102,22 @@ export default (props) => (
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiut aliquip ex ea commodo consequat.
             </div>
+
             <Grid columns="equal" className="tokens">
               <Grid.Row>
                 {tributes.map((token, idx) => (
-                  <Grid.Column key={idx}>
+                  <Grid.Column key={idx} mobile={16} tablet={8} computer={5} className="tributes">
                     <Segment className="pill" textAlign="center">
-                      <Icon name="ethereum" />{ token.amount } { token.currency }
+                      <Icon name="ethereum" />{token.amount} {token.currency}
                     </Segment>
                   </Grid.Column>
                 ))}
               </Grid.Row>
               <Grid.Row>
                 {tributes.map((token, idx) => (
-                  <Grid.Column key={idx}>
+                  <Grid.Column key={idx} mobile={16} tablet={8} computer={5} className="tributes">
                     <Segment className="pill" textAlign="center">
-                      <Icon name="ethereum" />{ token.amount } { token.currency }
+                      <Icon name="ethereum" />{token.amount} {token.currency}
                     </Segment>
                   </Grid.Column>
                 ))}
@@ -122,60 +129,68 @@ export default (props) => (
                 <p className="amount">$6,000</p>
               </Grid.Column>
               <Grid.Column textAlign="right">
-                <p className="subtext">Voting Shares (15%)</p>
+                <p className="subtext voting">Voting Shares (15%)</p>
                 <p className="amount">3,056,128</p>
               </Grid.Column>
             </Grid>
-          </Segment>
-        </Grid.Column>
+          </Grid.Column>
 
-        <Grid.Column width={3} />
+          <Grid.Column mobile={16} tablet={16} computer={2}   >
+            <Divider vertical />
+          </Grid.Column>
 
-        <Grid.Column width={7}>
-          <Grid columns="equal">
-            <Grid.Column floated="left">
-              <span className="pill">
-                <span className="subtext">Voting Ends:</span>&nbsp; 12 days
+          <Grid.Column mobile={16} tablet={16} computer={6} >
+
+            <Grid columns={16}>
+              <Grid.Column textAlign="left" mobile={16} tablet={8} computer={8} className="pill_column" >
+                <span className="pill">
+                  <span className="subtext">Voting Ends:</span>&nbsp; 12 days
               </span>
-            </Grid.Column>
-            <Grid.Column floated="right" textAlign="right" className="grace">
-              <span className="pill">
-                <span className="subtext">Grace Period:</span>&nbsp; 1 day
-              </span>
-            </Grid.Column>
-          </Grid>
-
-          <Grid columns="equal">
-            <Grid.Row className="members_row" centered>
-              { elders.map((elder, idx) => <MemberAvatar {...elder} key={idx} />) }
-              <Grid.Column textAlign="center">
-                <Button circular icon='caret down' color='grey' />
-                <p className="name">...</p>
               </Grid.Column>
-              
-            </Grid.Row>
-          </Grid>
+              <Grid.Column textAlign="right" className="pill_column grace" mobile={16} tablet={8} computer={8}>
+                <span className="pill">
+                  <span className="subtext">Grace Period:</span>&nbsp; 12 days
+              </span>
+              </Grid.Column>
+            </Grid>
+            <Grid columns={16} >
+              <Grid.Row>
+                <Grid.Column mobile={16} tablet={16} computer={16} className="pill_column"  >
+                  <Grid>
+                    <Grid.Row className="members_row" >
+                      {/* centered */}
+                      {elders.map((elder, idx) => <MemberAvatar {...elder} key={idx} />)}
 
-          <Grid>
-            <Grid.Column>
-              <ProgressBar yes={30} no={18}></ProgressBar>
-            </Grid.Column>
-          </Grid>
+                      <Grid.Column mobile={4} tablet={3} computer={3} textAlign="center" className="member_avatar">
+                        <Button className="caret_btn" circular centered icon='caret down' color='grey' />
+                        <p className="name">...</p>
+                      </Grid.Column>
+                    </Grid.Row>
+                  </Grid>
+                </Grid.Column >
+              </Grid.Row>
+            </Grid>
+            <Grid>
+              <Grid.Column>
+                <ProgressBar yes={30} no={18}></ProgressBar>
+              </Grid.Column>
+            </Grid>
 
-          <Grid columns="equal">
-            <Grid.Column>
-              <Button size='large' color='grey'>Vote No</Button>
-            </Grid.Column>
-            <Grid.Column>
-              <Button size='large' color='grey'>Vote Yes</Button>
-            </Grid.Column>
-            <Grid.Column>
-              <Button size='large' color='grey'>Process Proposal</Button>
-            </Grid.Column>
-          </Grid>
-        </Grid.Column>
+            <Grid columns="equal" centered>
+              <Grid.Column textAlign="center" mobile={16} tablet={5} computer={5} >
+                <Button className="btn" color='grey' >Vote No</Button>
+              </Grid.Column>
+              <Grid.Column textAlign="center" mobile={16} tablet={5} computer={5} >
+                <Button className="btn" color='grey' >Vote Yes</Button>
+              </Grid.Column>
+              <Grid.Column textAlign="center" mobile={16} tablet={5} computer={5} >
+                <Button className="btn" color='grey' >Process Proposal</Button>
+              </Grid.Column>
+            </Grid>
 
-      </Grid.Row>
+          </Grid.Column>
+        </Grid>
+      </Segment>
     </Grid>
   </div>
 )
